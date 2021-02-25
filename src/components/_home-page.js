044deea4497d43816e7ed907eba1bc9e82d15e1c
datnguyen <dat.nguyen@nfq.asia>
2021-02-25 23:28:39 +0700

    Add lazy load

--- src/components/_home-page.js
+++ src/components/_home-page.js
@@ -1,13 +1,40 @@
 import React, {Component} from 'react';
+import {LazyLoadImage} from 'react-lazy-load-image-component';
+
 
 export default class HomePage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {windowWidth: window.innerWidth};
+    }
+
+    handleResize = (e) => {
+        this.setState({windowWidth: window.innerWidth});
+    };
+
+    componentDidMount() {
+        window.addEventListener("resize", this.handleResize);
+    }
+
+    componentWillUnmount() {
+        window.addEventListener("resize", this.handleResize);
+    }
+
     render() {
+
+        const { windowWidth } = this.state;
 
         return (
             <div className="home-page text-center">
-                {[...Array(31)].map((item, key) => key !== 0 && <img key={key} src={`${process.env.PUBLIC_URL}/assets/images/${key}.png`}
-                                                        className="img-fluid menu-item" alt="Responsive image"/>)}
+                {[...Array(31)].map((item, key) => key !== 0 &&
+
+                    <LazyLoadImage key={key} src={`${process.env.PUBLIC_URL}/assets/images/${key}.png`}
+                                   effect="blur" alt="Responsive image" width={windowWidth}/>)}
             </div>
         )
     }
 }
+
+
+/*
+ */
